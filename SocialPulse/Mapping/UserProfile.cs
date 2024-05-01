97a91f37ef6bc95d2f4a1bad7fe6c3fbeb378c3d
Mo7hamedHany <124190342@sha.edu.eg>
2024-05-02 02:17:50 +0300

    profilepicCommit

---
+++ SocialPulse/Mapping/UserProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using SocialPulse.API.Helpers;
+using SocialPulse.Core.DtoModels.UserDto;
+using SocialPulse.Core.Models;
+
+namespace SocialPulse.API.Mapping
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User,UserDto>()
+                .ForMember(destination => destination.ProfilePicture , options => options.MapFrom<PictureUrlResolver>());
+        }
+    }
+}

b26b03ba4444e9bc00a86ef577d61f108de87106
Mo7hamedHany <124190342@sha.edu.eg>
2024-05-08 18:23:53 +0300

    kk

---
+++ SocilaPulse.Repository/Data/Seeding/Defaults.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialPulse.Repository.Data.Seeding
+{
+    public static class Defaults
+    {
+        public static string ProfilePicture = "images/users/DefaultProfilePic.png";
+    }
+}
